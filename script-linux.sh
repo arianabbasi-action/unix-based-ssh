77252ca00f6e246811c0d606d142161a883cd7bd
arian abbasi <97015797+arianabbasi-action@users.noreply.github.com>
2022-07-11 20:23:56 +0430

    Add files via upload

---
+++ script-linux.sh
@@ -0,0 +1,1 @@
+sudo apt update && sudo apt upgrade && sudo apt install ubuntu-desktop -y && sudo apt install xrdp -y && sudo adduser xrdp ssl-cert && sudo ufw enable && sudo ufw allow from 192.168.2.0/24 to any port 3389 && sudo ufw reload && sudo ufw status && echo Done!
